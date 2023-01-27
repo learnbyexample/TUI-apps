36b1eecaef02a105fae4b2010c1d5d88c19c243a
Sundeep Agarwal <learnbyexample.net@gmail.com>
2023-01-27 16:20:58 +0530

    support all re module functions

--- PyRegexPlayground/README.md
+++ PyRegexPlayground/README.md
@@ -7,7 +7,7 @@ This TUI application is intended as an interactive playground for Python Regular
 
 # Installation
 
-You'll need to install `textual` first. See [Textual documentation](https://textual.textualize.io/getting_started/) for more details about installation. After that, you can clone this repository and run the `pyregex_playground.py` script. Adjust terminal dimensions as needed. Example instructions shown below, adjust them based on your preferences and OS.
+You'll need to install `textual` first. See [Textual documentation](https://textual.textualize.io/getting_started/) for more details about installation. After that, you can clone this repository and run the `pyregex_playground.py` script. Adjust terminal dimensions as needed. Example instructions are shown below, adjust them based on your preferences and OS.
 
 ```bash
 $ python3 -m venv textual_apps
@@ -20,13 +20,26 @@ $ cd TUI-apps/PyRegexPlayground
 $ python pyregex_playground.py
 ```
 
-Here's a sample screenshot:
+Here are some sample screenshots:
 
-<p align="center"><img src="./pyregex_playground.png" alt="Sample screenshot for Python re(gex)? playground" /></p>
+<p align="center"><img src="./pyregex_sub.png" alt="Sample screenshot for the re.sub() function" /></p><br>
+
+<p align="center"><img src="./pyregex_finditer.png" alt="Sample screenshot for the re.finditer() function" /></p>
 
 # Guide
 
-You can type the search and replacement patterns in the input text boxes at the top of the application. Press **Enter** key to view the results. See Footer for customization options.
+You can type the search pattern in the **Compile** input box and press the **Enter** key to execute. For example, `re.compile(r'\d')` to match digit characters. Matching portions will be highlighted in red.
+
+The compiled pattern is available via the `pat` variable and you can use `ip` to refer to the input string. You can transform or extract data by typing appropriate methods in the **Action** box. For example, `pat.sub(r'(\g<0>)', ip)` will add parenthesis around the matching portions.
+
+The input string is obtained from the `ip.txt` file. You can change contents of this file and press **Ctrl+u** to update the `ip` variable. You'll have to press **Enter** again to update the results for the changed data.
+
+Some of the error types are caught and the background color of the input boxes will change to red. Other errors might result in the app crashing.
+
+Press **Ctrl+t** to toggle the theme between light and dark modes.
+
+> **Warning**  
+> There is no safeguard against the commands you have typed. They are treated as if you executed them from a Python program.
 
 # Ebook
 
--- PyRegexPlayground/pyregex_playground.py
+++ PyRegexPlayground/pyregex_playground.py
@@ -3,8 +3,8 @@ from textual.binding import Binding
 from textual.containers import Horizontal
 from textual.widgets import Footer, Label, Input
 from rich.panel import Panel
-from rich.markup import escape
 from rich.text import Text
+from rich.pretty import Pretty
 
 import re
 from functools import partial
@@ -12,8 +12,7 @@ from functools import partial
 class PyRegexPlayground(App):
     CSS_PATH = 'pyregex_playground.css'
     BINDINGS = [
-        Binding('ctrl+r', 'replace', 'Replace', show=True),
-        Binding('ctrl+p', 'prettify', 'Prettify', show=True),
+        Binding('ctrl+u', 'update_ip', 'Update ip', show=True),
         ('ctrl+t', 'toggle_theme', 'Theme'),
         ('ctrl+q', 'app.quit', 'Quit'),
     ]
@@ -21,85 +20,72 @@ class PyRegexPlayground(App):
     def __init__(self):
         super().__init__()
 
-        self.l_search = Label('Search', id='l_search')
-        self.l_replace = Label('Replace', id='l_replace')
+        self.l_compile = Label('Compile', id='l_compile')
+        self.l_action = Label('Action', id='l_action')
 
-        self.data = ('Apple Banana Cherry\n'
-                     'coffee\t: 150g\n'
-                     'tea\t: 50g\n'
-                     'sugar\t: 10g\n'
-                     'Dragon Unicorn Centaur')
-        self.prettify = True
-
+        self.read_data()
         self.l_input = Label(id='l_input')
-        self.l_input.update(Panel(self.data,
-                                  title='Input',
-                                  title_align='center'))
+        self.ip_panel = partial(Panel, title='ip', title_align='center')
+        self.l_input.update(self.ip_panel(self.data))
 
         self.l_output = Label(id='l_output')
-        self.l_output.update(Panel('',
-                                   title='Output',
-                                   title_align='center'))
+        self.op_panel = partial(Panel, title='Output', title_align='center')
+        self.l_output.update(self.op_panel(''))
 
-        placeholder = 'Search pattern. Press Enter to execute the command.'
-        self.i_search = Input(placeholder=placeholder)
-        self.i_search.styles.background = 'lightgray'
-        placeholder = 'Replace pattern. Press Enter to execute the command.'
-        self.i_replace = Input(placeholder=placeholder)
-        self.i_replace.styles.background = 'lightgray'
+        placeholder = 'Search pattern. Press Enter to compile.'
+        self.i_compile = Input(placeholder=placeholder, value="re.compile(r'')")
+        self.i_compile.styles.background = 'lightgray'
+        placeholder = "Function to run. Example: pat.sub(r'X', ip)"
+        self.i_action = Input(placeholder=placeholder)
+        self.i_action.styles.background = 'lightgray'
+
+        self.error_types = (re.error, SyntaxError, TypeError, ValueError,
+                            NameError, AttributeError)
 
     def compose(self):
         yield Label('[b]Python re(gex)? playground', id='header')
-        yield Horizontal(self.l_search, self.i_search, id='h_search')
-        self.h_replace = Horizontal(self.l_replace, self.i_replace, id='h_replace')
-        yield self.h_replace
+        yield Horizontal(self.l_compile, self.i_compile, id='h_compile')
         yield self.l_input
+        yield Horizontal(self.l_action, self.i_action, id='h_action')
         yield self.l_output
         yield Footer()
 
     def on_mount(self):
         self.dark = False
-        self.h_replace.visible = False
-        self.l_output.visible = self.h_replace.visible
+        self.i_compile.focus()
+        self.i_compile.cursor_position = len(self.i_compile.value) - 2
 
     async def on_input_submitted(self, message):
-        self.execute()
-
-    def execute(self):
-        self.execute_search()
-        if self.h_replace.visible:
-            self.execute_replace()
-
-    def substitute(self, ip, pat, color):
-        # need to take care of rich markup in data/patterns
-        # or find another way to color
-        if self.prettify:
-            pat = f'[b][{color}]{pat}[/{color}][/b]'
-
+        ip = self.data
         try:
-            ip.styles.background = 'lightgray'
-            op = re.sub(self.i_search.value, pat, self.data)
-            return Text.from_markup(op) if self.prettify else op
-        except re.error:
-            ip.styles.background = 'red'
-            return self.data
+            self.i_compile.styles.background = 'lightgray'
+            pat = eval(self.i_compile.value)
+        except self.error_types:
+            self.i_compile.styles.background = 'red'
+        else:
+            op = Text(ip)
+            for m in pat.finditer(ip):
+                op.stylize('bold red', *m.span())
+            self.l_input.update(self.ip_panel(op))
 
-    def execute_search(self):
-        op = self.substitute(self.i_search, r'\g<0>', 'red')
-        self.l_input.update(Panel(op, title='Input', title_align='center'))
+        if self.i_action.value:
+            try:
+                self.i_action.styles.background = 'lightgray'
+                op = eval(self.i_action.value)
+            except self.error_types:
+                self.i_action.styles.background = 'red'
+            else:
+                if type(op) != str:
+                    op = Pretty(op)
+                self.l_output.update(self.op_panel(op))
 
-    def execute_replace(self):
-        op = self.substitute(self.i_replace, self.i_replace.value, 'green')
-        self.l_output.update(Panel(op, title='Output', title_align='center'))
+    def read_data(self):
+        with open('ip.txt') as f:
+            self.data = f.read()
 
-    def action_replace(self):
-        self.h_replace.visible = not self.h_replace.visible
-        self.l_output.visible = self.h_replace.visible
-        self.execute_replace()
-
-    def action_prettify(self):
-        self.prettify = not self.prettify
-        self.execute()
+    def action_update_ip(self):
+        self.read_data()
+        self.l_input.update(self.ip_panel(self.data))
 
     def action_toggle_theme(self):
         self.dark = not self.dark
